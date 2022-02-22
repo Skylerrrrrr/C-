52129fa623bcbfeafcb2806900d50c1285415950
Skylerrrrrr <qwesdfjklop@gmail.com>
2022-02-22 17:19:47 +0800

    更新笔记

--- first.c
+++ first.c
@@ -1,4 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 
+int main()//main是一个函数名//int表示的是main函数返回一个整数
+{
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
+/* 数据类型：
+    char: 声明字符型变量或函数
+    double: 声明双精度变量或函数
+    float: 声明浮点型变量或函数
+    int : 声明整型变量或函数
+    long: 声明长整型变量或函数
+    short : 声明短整型变量或函数
+    signed : 声明有符号类型变量或函数
+    _Bool:  声明布尔类型
+    _Complex ：声明复数
+    _Imaginary: 声明虚数
+    unsigned : 声明无符号类型变量或函数
+    void : 声明函数无返回值或无参数，声明无类型指针
+*/
+
+/* 控制语句：
+
+    -循环语句
+
+for : for 循环，使用的最多
+do ：循环语句的前提条件循环体
+while：循环语句的循环条件
+break : 跳出当前循环
+continue：结束当前循环，开始下一轮循环
+
+    -条件语句
+
+if：条件语句的判断条件
+else : 条件语句的否定分支，与 if 连用
+goto: 无条件跳转语句
+
+    -开关语句
+
+switch: 用于开关语句
+case：开关语句的另外一种分支
+default : 开关语句中的其他分支
+
+    -返回语句
+return ：子程序返回语句（可以带参数，也看不带参数） 
+*/
 
