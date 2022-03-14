32dc2969b046d4c3bce5bfcc08d21f97feb04581
Skylerrrrrr <qwesdfjklop@gmail.com>
2022-03-14 22:13:43 +0800

    斐波那契数列

--- Test/src/main.c
+++ Test/src/main.c
@@ -928,24 +928,68 @@ int main()
 //     return 0;
 // }
 
-//求n的阶乘
+/* //求n的阶乘
 int Fac1(int n)
 {
     int i = 0;
     int ret = 1;
-    for (i=1; i<=n, i++)
+    for (i=1; i<=n; i++)
     {
-        
+        ret *= i;
     }
+    return ret;
 }
-
+int Fac2(int n)
+{
+    if(n<=1)
+        return 1;
+    else
+        return n*Fac2(n-1);
+}
 int main()
 {
     int n = 0;
     int ret = 0;
     scanf("%d", &n);
-    ret = Fac1(n);
+    ret = Fac2(n);
     printf("%d\n", ret);
     return 0;
+} */
+
+/* //斐波那契数列(1 1 2 3 5 8 13 21 34 55...)
+//描述第n个斐波那契数
+//这种方法效率极低, 因为重复计算了
+int Fib(int n)
+{
+    if(n<=2)
+        return 1;
+    else
+        return Fib(n-1)+Fib(n-2); 
 }
+//method 2(找规律):
+int Fib2(int n)
+{
+     int a = 1;
+     int b = 1;
+     int c = 1;
+
+     while(n>2)
+     {
+         c = a+b;
+         a = b;
+         b = c;
+         n--;
+     }
+return c;
+
+}
+int main()
+{
+    int n = 0;
+    int ret = 0;
+    scanf("%d", &n);
+    ret = Fib2(n);
+    printf("ret = %d\n", ret);
+    return 0;
+} */
 
